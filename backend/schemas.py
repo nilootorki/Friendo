6ee109f316a0c1b7a30d49cb9266bf339cdae0a1
Rojan Jaleh <rojanjaleh22@gmail.com>
2025-02-26 00:27:09 -0800

    implement endpoints to get contacts

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,13 +1,13 @@
 #data validation to ensure that data we store, follows a proper format
 from pydantic import BaseModel ,   #pydantiic is a data validation and serialization library that FastAPII uses to ensure data is valid and structured properly
-from typing import Optional
+from typing import Optional,List,Dict
 
 #define Pydantic models
 
 class FriendsBase(BaseModel):   #defines the fields that should exist in every contact objects
     name:str
-    phone_number:Optional[str]   #not sure to add these
-    emaiL:Optional[str]
+    phone_number:Optional[str]=None   #not sure to add these
+    emaiL:Optional[str] =None
     
     
     
