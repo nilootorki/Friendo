ae53abc7dc01d49d7a8924de27bba4621df957a3
Niloofar Torki <110784444+nilootorki@users.noreply.github.com>
2025-02-27 18:02:41 +0330

    Suggestion has been added!not connected to database yet!

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,6 +1,9 @@
 #data validation to ensure that data we store, follows a proper format
 from pydantic import BaseModel   #pydantiic is a data validation and serialization library that FastAPII uses to ensure data is valid and structured properly
 from typing import Optional,List,Dict
+from datetime import datetime
+from typing import Any, Dict
+
 
 #define Pydantic models
 
@@ -15,4 +18,18 @@ class FriendsResponse(FriendsBase):
     class config:
         orm_mode=True
         
+
+class UserFriendSchema(BaseModel):
+    id: int
+    user_id: int
+    friend_id: int
+    interaction_type: str
+    timestamp: datetime
+    messages: Dict[str, Any]
+    score: List[Dict[str, Any]]
+
+
+
+    class Config:
+        from_attributes = True
         
